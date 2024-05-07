b84fe075a067ee6a332755feb918ed122fbb36ff
Clara Rigaud <clararigaud0@gmail.com>
2024-05-07 10:30:09 +0200

    ajout main.js

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+import './vendors/lightbox';
+import './vendors/carousel';
+import './theme/';
+
+// devrait etre dans le theme noesya non ?
+import './design-system/footer';
+import './design-system/menu';
