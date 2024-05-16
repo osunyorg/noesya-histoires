9f054d1f0d31f8675dca73850144391edcb81a4f
Clara Rigaud <clararigaud0@gmail.com>
2024-05-16 17:28:50 +0200

    ES5 ouais

--- assets/js/constellation.js
+++ assets/js/constellation.js
@@ -8,8 +8,8 @@ function MenuCanvas(canvas, liste) {
         var nom = t.shift();
         var reste = t.join(" ");
         //console.log(nom,": ", reste);
-      }
-  
+    }
+
     // Positions en pourcentage par rapport à la taille actuelle du canvas
     this.positions = [
         { x: 53, y: 18, connected: [1]},
@@ -35,7 +35,6 @@ MenuCanvas.prototype.init = function(){
 MenuCanvas.prototype.draw = function(){
 
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    // this.particleArray.forEach(p => p.resetConnections()); //connections entre particules
     
     for (var i = 0; i < this.particleArray.length; i++) {
         var particle = this.particleArray[i];
@@ -55,52 +54,47 @@ MenuCanvas.prototype.draw = function(){
     }
 }
 
-class Particle { //passer ne ES5
-    constructor(canvas, pos) {
-        this.size = Math.random() * 10 + 10;
-        this.directionX = Math.random();
-        this.directionY = Math.random();
-        this.isHovered = false;
-        this.x= canvas.width * (pos.x / 100);
-        this.y= canvas.height * (pos.y / 100);
-        this.connections =  pos.connected;
+function Particle(canvas, pos) {
+    this.size = Math.random() * 10 + 10;
+    this.directionX = Math.random();
+    this.directionY = Math.random();
+    this.isHovered = false;
+    this.x= canvas.width * (pos.x / 100);
+    this.y= canvas.height * (pos.y / 100);
+    this.connections =  pos.connected;
+}
+    
+Particle.prototype.update = function(particles, canvas) {
+    this.x += this.directionX * 0.00002;
+    this.y += this.directionY * 0.00002;
+    
+    //Créer limites de la constellation
+    var centerX = canvas.width / 2;
+    var centerY = canvas.height / 2;
+    var halfDiagonalX = Math.min(canvas.width, canvas.height) / 12;
+    var halfDiagonalY = Math.min(canvas.width, canvas.height) / 6;
+
+    //reste dans les limites
+    var influence = ((Math.abs(this.x - centerX) / halfDiagonalX) + (Math.abs(this.y - centerY) / halfDiagonalY)) - 1;
+    if (influence > 0) {
+        this.directionX -= (influence * (this.x - centerX) / halfDiagonalX );
+        this.directionY -= (influence * (this.y - centerY) / halfDiagonalY );
     }
     
-    update(Particles, canvas) {
-        this.x += this.directionX * 0.00002;
-        this.y += this.directionY * 0.00002;
-        
-        //Créer limites de la constellation
-        var centerX = canvas.width / 2;
-        var centerY = canvas.height / 2;
-        var halfDiagonalX = Math.min(canvas.width, canvas.height) / 12;
-        var halfDiagonalY = Math.min(canvas.width, canvas.height) / 6;
-
-        //reste dans les limites
-        var influence = ((Math.abs(this.x - centerX) / halfDiagonalX) + (Math.abs(this.y - centerY) / halfDiagonalY)) - 1;
-        if (influence > 0) {
-            this.directionX -= (influence * (this.x - centerX) / halfDiagonalX );
-            this.directionY -= (influence * (this.y - centerY) / halfDiagonalY );
-        }
-        
-        //Les particules se repoussent les unes les autres
-        Particles.forEach(particle => {
-            if (particle !== this) {
-                var dx = particle.x - this.x;
-                var dy = particle.y - this.y;
-                var distance = Math.sqrt(dx * dx + dy * dy);
-                if (distance < 300) {
-                    var repulsion = 1;
-                    this.directionX -= (dx * repulsion)/50;
-                    this.directionY -= (dy * repulsion)/50;
-                }
+    //Les particules se repoussent les unes les autres
+    for(var i = 0; i<particles.length; i++){
+        var particle = particles[i];
+        if (particle !== this) {
+            var dx = particle.x - this.x;
+            var dy = particle.y - this.y;
+            var distance = Math.sqrt(dx * dx + dy * dy);
+            if (distance < 300) {
+                var repulsion = 1;
+                this.directionX -= (dx * repulsion)/50;
+                this.directionY -= (dy * repulsion)/50;
             }
-        });
+        }
+
     }
-
-    // resetConnections() {
-    //     this.connections = 0;
-    // }
 }
-
 export default MenuCanvas;
