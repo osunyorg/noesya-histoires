5666ac3f50d820e9474d24aaedad5ddfd674d3bc
Alexis BENOIT <alex@noesya.coop>
2025-12-03 12:34:34 +0100

    v8

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,8 @@ import './theme/';
 import './design-system/footer';
 import './design-system/menu';
 import './osuny-plugin-single-page-application/main';
+
+window.osuny = window.osuny || {};
+window.osuny.swupOptions = {
+    containers: ['#main', '#document-header .nav-level-1']
+};
